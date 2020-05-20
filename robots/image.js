dab126f5754f607c70140b1557cdbb227b2306c2
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2020-05-19 22:33:24 -0300

    feat: add image download logic with fallback

--- robots/image.js
+++ robots/image.js
@@ -1,3 +1,4 @@
+const imageDownloader = require('image-downloader')
 const google = require('googleapis').google
 const customSearch = google.customsearch('v1')
 const state = require('./state')
@@ -9,8 +10,10 @@ async function robot() {
 
   await fetchImageOfAllSentences(content)
 
+  await downloadAllImages(content)
+
   state.save(content)
-  
+
   async function fetchImageOfAllSentences(content) {
     for(const sentence of content.sentences) {
       const query = `${content.searchTerm} ${sentence.keywords[0]}`
@@ -33,6 +36,35 @@ async function robot() {
     })
 
     return imageUrl
+  }
+
+  async function downloadAllImages(content) {
+    content.downloadedImages = []
+
+    for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+      const images = content.sentences[sentenceIndex].images
+
+      for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+        const imageUrl = images[imageIndex]
+
+        try {
+ 
+          await downloadAndSave(imageUrl, `${sentenceIndex}-original.png`)
+          content.downloadedImages.push(imageUrl)
+          console.log(`>[robo-image] [${sentenceIndex}] [${imageIndex}] Image successfully downloaded: ${imageUrl}`)
+          break
+        } catch(error) {
+          console.log(`> [robo-image] [${sentenceIndex}] [${imageIndex}] Erro ao baixar (${imageUrl}): ${error}`)
+        }
+      }
+    }
+  }
+
+  async function downloadAndSave(url, fileName) {
+    return imageDownloader.image({
+      url: url,
+      dest: `./content/${fileName}`
+    })
   }
 }
 
